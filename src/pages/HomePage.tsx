f269da9e3b27f5017db950f45ee2397859543cee
JoJoseph <sephtianyt@gmail.com>
2024-11-04 17:23:48 +0700

    Feat: Home Page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Hero from '../components/Hero';
+import Project from '../components/Project';
+
+function HomePage() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
+
+export default HomePage;
