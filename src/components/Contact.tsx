1a84255bc4ef201486313ba71271a0c06a145658
JoJoseph <sephtianyt@gmail.com>
2024-11-07 05:26:51 +0700

    Fix: wrong data

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -25,7 +25,7 @@ function Contact() {
             <ContactItem isLink link="https://linkedin.com/in/joseph-christian-a5b299271" icon={<AiOutlineLinkedin className="h-6 w-6" />} title="Linked In" detail="Joseph Christian" />
           </motion.div>
           <motion.div variants={variantsFadeInUp}>
-            <ContactItem isLink={false} link="" icon={<MdOutlineAttachEmail className="h-6 w-6" />} title="Email" detail="sephtianyt@gmail.com" />
+            <ContactItem isLink={false} link="" icon={<MdOutlineAttachEmail className="h-6 w-6" />} title="Email" detail="josephjcwork@gmail.com" />
           </motion.div>
           <motion.div variants={variantsFadeInUp}>
             <ContactItem isLink link="https://discordapp.com/users/343581363745718273" icon={<AiOutlineDiscord className="h-6 w-6" />} title="Discord" detail="sephtian" />
