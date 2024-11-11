d674fc9ca7efa14d27ade51755f6b65480a6f957
JoJoseph <sephtianyt@gmail.com>
2024-11-11 19:50:10 +0700

    Style: Add new project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -14,6 +14,7 @@ function Project() {
       </header>
       <motion.article variants={variants} initial={hidden} whileInView={visible} viewport={{ once: true }} className="grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         <ProjectItem link="https://josephjc.vercel.app/" image="/images/jc-personal-website.jpg" stacks={['typescript', 'react', 'tailwind']} title="JC. - Personal Website" />
+        <ProjectItem link="https://jc-world-thread-app.vercel.app/" image="/images/wtc.jpg" stacks={['react', 'tailwind']} title="World Thread Center" />
       </motion.article>
     </section>
   );
